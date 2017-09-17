944f8253c92dc9bb9a5d38c1e424cb5794123697
Andy Chen <amazingandyyy@gmail.com>
2017-09-16 17:36:44 -0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = express()
 
 var bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+var db = {};
 
 app.get('/', function (req, res) {
   res.send({
@@ -15,11 +17,18 @@ app.get('/', function (req, res) {
       "repo": "https://github.com/amazingandyyy/jaj-server"
     }
   });
-})
+});
+
+app.get('/msg/:data', function(req, res) {
+  // var info = JSON.parse(req.query);
+  console.log('data', req.params.data);
+  res.send(req.params.data);
+});
 
 app.get('/incoming-sms', function(req, res) {
-  console.log(req.query);
+  var info = JSON.parse(req.query);
+  console.log('info', req.query);
   res.sendStatus(200);
-})
+});
 
-app.listen(3000);
+app.listen(4040);
